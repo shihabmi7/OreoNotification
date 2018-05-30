5c1e04c5d913bfcf8f1310f892ace48ab21bf1c1
DESKTOP-53A39RD\User <shihab.apps.developer@gmail.com>
2018-05-30 11:28:39 +0600

    Initial commit

---
+++ app/src/main/java/me/shihab/oreonotification/HomeActivity.java
@@ -0,0 +1,71 @@
+package me.shihab.oreonotification;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private static final String CHANNEL_ID = "100";
+    private static final CharSequence CHANNEL_NAME = "Game";
+    private static final int NOTIFICATION_ID = 501;
+    Button button_show_notification;
+
+    NotificationManager notificationManager;
+    NotificationChannel notificationChannel;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        button_show_notification = (Button) findViewById(R.id.button_show_notification);
+
+
+        notificationManager = (NotificationManager) getSystemService
+                (Context.NOTIFICATION_SERVICE);
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+
+            notificationChannel = new
+                    NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH);
+            notificationChannel.setDescription("Game related notifications shows in this channel");
+            notificationChannel.enableVibration(true);
+
+            // your notification channel is ready. you can add several criteria to here like
+        }
+
+        // Create an explicit intent for an Activity in your app
+        Intent intent = new Intent(this, HomeActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle("Brazil vs Germany")
+                .setContentText("Today is world cup final!!!")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                // Set the intent that will fire when the user taps the notification
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        final Notification notification = mBuilder.build();
+
+        button_show_notification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                notificationManager.notify(NOTIFICATION_ID, notification);
+
+            }
+        });
+    }
+}
